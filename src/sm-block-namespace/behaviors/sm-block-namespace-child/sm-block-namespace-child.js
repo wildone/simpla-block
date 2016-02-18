0888d4d6f70b47552828571e90d95cf1580be47f
Bede <bede.overend@gmail.com>
2016-02-18 17:53:07 +1100

    change behavior to expect uid property

--- src/sm-block-namespace/behaviors/sm-block-namespace-child/sm-block-namespace-child.js
+++ src/sm-block-namespace/behaviors/sm-block-namespace-child/sm-block-namespace-child.js
@@ -4,6 +4,23 @@ window.simpla.behaviors = window.simpla.behaviors || {};
 
 /**
  * A behavior for elements that can live in namespaces.
+ *
+ * It expects you to already have a property named UID:
+ *
+ *  This is the unique id, and is scoped to the project. It will be based on
+ *  	on the namespace ancestry, and the current sid. It's is merely the path from
+ *    this element all the way back to the root ns, passing through each namespace.
+ *
+ * 	In the example below, the base url and therefore root ns is http://foobar.com,
+ *   the second and third namespaces are bar and baz respectively. This pid is qux
+ *   Example: 'http://foobar\.com.bar.baz.qux'
+ *
+ * 	More simply, it is `${namespace.uid}.${this.sid}`
+ *
+ * 	Note: It is updated via observers and event handlers, as it needs to be
+ *  	manually mutated by internal methods, it's not computed
+ *
+ *
  * @type {Object}
  */
 const blockNamespaceChild = {
@@ -15,28 +32,6 @@ const blockNamespaceChild = {
      */
     sid: {
       type: String,
-      value: ''
-    },
-
-    /**
-     * This is the unique id, and is scoped to the project. It will be based on
-     * 	on the namespace ancestry, and the current sid. It's is merely the path from
-     *  this element all the way back to the root ns, passing through each namespace.
-     *
-     * In the example below, the base url and therefore root ns is http://foobar.com,
-     *  the second and third namespaces are bar and baz respectively. This pid is qux
-     * Example: 'http://foobar\.com.bar.baz.qux'
-     *
-     * More simply, it is `${namespace.uid}.${this.sid}`
-     *
-     * Note: It is updated via observers and event handlers, as it needs to be
-     * 	manually mutated by internal methods, it's not computed
-     *
-     * @type {String}
-     */
-    uid: {
-      type: String,
-      readOnly: true,
       value: ''
     },
 
@@ -72,7 +67,7 @@ const blockNamespaceChild = {
     if (!this._namespaceListener) {
       this._namespaceListener = (event) => {
         let uid = event.detail.value;
-        this._setUid(this._computeUid(uid, this.sid));
+        this._changeUid(this._computeUid(uid, this.sid));
       }
     }
 
@@ -84,6 +79,19 @@ const blockNamespaceChild = {
     if (previousNamespace) {
       previousNamespace.removeElement(this);
       previousNamespace.removeEventListener('uid-changed', this._namespaceListener);
+    }
+  },
+
+  /**
+   * Helper function to set a uid, whether it's read only or not
+   * @param  {String} uid UID to set this.uid to
+   * @return {undefined}
+   */
+  _changeUid(uid) {
+    if (this._setUid) {
+      this._setUid(uid);
+    } else {
+      this.uid = uid;
     }
   },
 
@@ -103,7 +111,7 @@ const blockNamespaceChild = {
     let base = namespace ? namespace.uid : '',
         uid = this._computeUid(base, sid);
 
-    this._setUid(uid);
+    this._changeUid(uid);
   },
 
   /**
